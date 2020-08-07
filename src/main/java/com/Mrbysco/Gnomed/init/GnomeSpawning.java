7d20d871078cc100b6fa2aa0956694553d96260c
Mrbysco <Mrbysco@users.noreply.github.com>
2020-08-07 23:14:45 +0200

    Fix

--- src/main/java/com/Mrbysco/Gnomed/init/GnomeSpawning.java
+++ src/main/java/com/Mrbysco/Gnomed/init/GnomeSpawning.java
@@ -1,8 +1,9 @@
 package com.mrbysco.gnomed.init;
 
-import com.mrbysco.gnomed.entities.GnomeEntity;
+import com.mrbysco.gnomed.Gnomed;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.common.BiomeDictionary;
@@ -13,10 +14,10 @@ public class GnomeSpawning {
 
 	public static void addSpawn() {
 		for(Biome biome : ForgeRegistries.BIOMES) {
-			if(BiomeDictionary.hasType(biome, Type.FOREST)) {
-				biome.getSpawns(EntityClassification.CREATURE).add(new Biome.SpawnListEntry(GnomeRegistry.GNOME.get(), 12, 1, 4));
+			if(BiomeDictionary.hasType(biome, Type.FOREST) || BiomeDictionary.hasType(biome, Type.JUNGLE)) {
+				biome.getSpawns(EntityClassification.CREATURE).add(new Biome.SpawnListEntry(GnomeRegistry.GNOME.get(), 12, 1, 1));
 			}
 		}
-		EntitySpawnPlacementRegistry.register(GnomeRegistry.GNOME.get(), EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, GnomeEntity::canSpawnOn);
+		EntitySpawnPlacementRegistry.register(GnomeRegistry.GNOME.get(), EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MobEntity::canSpawnOn);
 	}
 }
