2b730cda8f85f2aeb6d5dc71a656a764c601fcab
Mrbysco <Mrbysco@users.noreply.github.com>
2023-03-30 23:33:28 +0200

    Fix datagen [build] [publish]

--- src/main/java/com/mrbysco/gnomed/datagen/GnomeDataGen.java
+++ src/main/java/com/mrbysco/gnomed/datagen/GnomeDataGen.java
@@ -1,11 +1,8 @@
 package com.mrbysco.gnomed.datagen;
 
-import com.google.gson.JsonElement;
-import com.mojang.serialization.JsonOps;
 import com.mrbysco.gnomed.Reference;
 import com.mrbysco.gnomed.init.GnomeRegistry;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -15,12 +12,11 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.registries.VanillaRegistries;
-import net.minecraft.resources.RegistryOps;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MobSpawnSettings.SpawnerData;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
@@ -31,8 +27,7 @@ import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.common.data.JsonCodecProvider;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers.AddSpawnsBiomeModifier;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -44,41 +39,44 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GnomeDataGen {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
-		HolderLookup.Provider provider = getProvider();
-		final RegistryOps<JsonElement> ops = RegistryOps.create(JsonOps.INSTANCE, provider);
 		DataGenerator generator = event.getGenerator();
 		PackOutput packOutput = generator.getPackOutput();
-		ExistingFileHelper helper = event.getExistingFileHelper();
 
 		if (event.includeServer()) {
 			generator.addProvider(event.includeServer(), new GnomeLoot(packOutput));
 
-			final HolderLookup.RegistryLookup<Biome> biomeReg = provider.lookupOrThrow(Registries.BIOME);
-			final BiomeModifier addSpawn = AddSpawnsBiomeModifier.singleSpawn(
-					HolderSet.emptyNamed(biomeReg, BiomeTags.IS_FOREST),
-					new SpawnerData(GnomeRegistry.GNOME.get(), 12, 1, 1));
-
-			final BiomeModifier addJungleSpawn = AddSpawnsBiomeModifier.singleSpawn(
-					HolderSet.emptyNamed(biomeReg, BiomeTags.IS_JUNGLE),
-					new SpawnerData(GnomeRegistry.GNOME.get(), 12, 1, 1));
-
-			generator.addProvider(event.includeServer(), JsonCodecProvider.forDatapackRegistry(
-					packOutput, helper, Reference.MOD_ID, ops, ForgeRegistries.Keys.BIOME_MODIFIERS,
-					Map.of(new ResourceLocation(Reference.MOD_ID, "add_forest_gnome_spawn"), addSpawn,
-							new ResourceLocation(Reference.MOD_ID, "add_jungle_gnome_spawn"), addJungleSpawn
-					)
-			));
+			generator.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(
+					packOutput, CompletableFuture.supplyAsync(GnomeDataGen::getProvider), Set.of(Reference.MOD_ID)));
 		}
 	}
 
+	public static final ResourceKey<BiomeModifier> ADD_FOREST_GNOME_SPAWN = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS,
+			new ResourceLocation(Reference.MOD_ID, "add_forest_gnome_spawn"));
+	public static final ResourceKey<BiomeModifier> ADD_JUNGLE_GNOME_SPAWN = ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS,
+			new ResourceLocation(Reference.MOD_ID, "add_jungle_gnome_spawn"));
+
 	private static HolderLookup.Provider getProvider() {
 		final RegistrySetBuilder registryBuilder = new RegistrySetBuilder();
+		registryBuilder.add(ForgeRegistries.Keys.BIOME_MODIFIERS, context -> {
+			var biomeLookup = context.lookup(Registries.BIOME);
+
+			final BiomeModifier addSpawn = AddSpawnsBiomeModifier.singleSpawn(
+					biomeLookup.getOrThrow(BiomeTags.IS_FOREST),
+					new SpawnerData(GnomeRegistry.GNOME.get(), 12, 1, 1));
+			context.register(ADD_FOREST_GNOME_SPAWN, addSpawn);
+
+			final BiomeModifier addJungleSpawn = AddSpawnsBiomeModifier.singleSpawn(
+					biomeLookup.getOrThrow(BiomeTags.IS_JUNGLE),
+					new SpawnerData(GnomeRegistry.GNOME.get(), 12, 1, 1));
+			context.register(ADD_JUNGLE_GNOME_SPAWN, addJungleSpawn);
+		});
 		// We need the BIOME registry to be present so we can use a biome tag, doesn't matter that it's empty
 		registryBuilder.add(Registries.BIOME, $ -> {
 		});
